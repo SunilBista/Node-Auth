78fcb60feaff2fc96d31b5c8e6ed978cdf3e8c35
SunilBista <sunilbista303@gmail.com>
2024-09-21 01:23:24 +1000

    feat: set and read cookie

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const dbConnect = require("./config/dbConnect");
 const appRoutes = require("./routes/authRoutes");
+const cookieParser = require("cookie-parser");
+
 dbConnect()
   .then(() => {
     const port = process.env.PORT || 3000;
@@ -17,8 +19,22 @@ app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(appRoutes);
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   console.log("Home");
   res.render("home");
+});
+
+app.get("/set-cookies", (req, res) => {
+  res.cookie("newUser", false);
+  res.cookie("isEmployee", false, { maxAge: 100000 });
+  res.send("you got the cookies");
+});
+
+app.get("/read-cookies", (req, res) => {
+  const cookies = req.cookies;
+  console.log(cookies.newUser);
+
+  res.json(cookies);
 });
