a2f031cc188f71593818c4d543ce1c81119ed89d
SunilBista <sunilbista303@gmail.com>
2024-09-21 03:02:34 +1000

    feat: add logout handler

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,11 +2,13 @@ const express = require("express");
 const router = express.Router(); //new instance of router object - small router
 const authController = require("../controllers/authController");
 
-const { signup_get, login_get, signup_post, login_post } = authController;
+const { signup_get, login_get, signup_post, login_post, logout_get } =
+  authController;
 
 router.get("/signup", signup_get);
 router.post("/signup", signup_post);
 router.get("/login", login_get);
 router.post("/login", login_post);
+router.get("/logout", logout_get);
 
 module.exports = router;
