a0fb2245fa496c10e91904872a47c6c35460305c
SunilBista <sunilbista303@gmail.com>
2024-09-20 15:39:48 +1000

    feat: add error handlers and db value validator

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,26 @@
 const User = require("../models/User");
+
+const handleErrors = (err) => {
+  console.log(err.message, err.code);
+  let errors = {
+    email: "",
+    password: "",
+  };
+
+  //duplicate error code
+  if (err?.code === 11000) {
+    errors.email = "Duplicate email";
+    return errors;
+  }
+
+  if (err?.message.includes("user validation failed")) {
+    Object.values(err?.errors).forEach(({ properties }) => {
+      errors[properties.path] = properties.message;
+    });
+  }
+  return errors;
+};
+
 const signup_get = (req, res) => {
   res.render("signup");
 };
@@ -15,10 +37,9 @@ const signup_post = async (req, res) => {
       password,
     });
     res.status(201).json(user);
-    console.log("New user signup");
   } catch (err) {
-    console.log(err);
-    res.status(400).send("Error, user not created");
+    const errors = handleErrors(err);
+    res.status(400).json({ errors });
   }
 };
 
